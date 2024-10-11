02159e91d390ffb47f946ff7b840a028048836fe
Zachary Springer <zdspringer99@gmail.com>
2024-10-11 11:07:13 -0700

    added a try/catch block

--- src/loopymaps.java
+++ src/loopymaps.java
@@ -1,7 +1,20 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 
 public static void main(String args[]) throws FileNotFoundException {
-    Scanner scanner = new Scanner(new FileReader("legions"));
+    BufferedReader reader = new BufferedReader(new FileReader("legions"));
+    Map<String, String> traitorMap = new LinkedHashMap<>();
+    Map<String, String> loyalMap = new LinkedHashMap<>();
+
+    try (reader) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(",");
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
 }
